5a41b495320906de4740df4033589264306ed69f
Evgenyi Bagmut PC <jenya.bagmyt@yandex.ru>
2023-09-21 18:22:50 +0300

    Please enter the commit message for your changes. Lines starting

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -4,7 +4,15 @@
 // Console.WriteLine(numberA + numberB);
 
 
-int numberA = 10;
-int numberB = 5;
+// int numberA = 10;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 5 .... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
