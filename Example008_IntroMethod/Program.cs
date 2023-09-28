af50ecafe67f2083413154d5929d6d29d5c17566
Evgenyi Bagmut PC <jenya.bagmyt@yandex.ru>
2023-09-28 16:42:35 +0300

    Добавлен пример 8.1

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -9,19 +9,26 @@
 
 int a1 = 11;
 int b1 = 21;
-int c1 = 31;
+int c1 = 31999;
 int a2 = 77;
 int b2 = 22;
 int c2 = 32;
-int a3 = 13;
+int a3 = 13225;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
 
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+
+Console.WriteLine(max);
 // int max = a1;
 // if(b1 > max ) max = b1;
 // if(c1 > max ) max = c1;
@@ -34,4 +41,3 @@ int max = Max(max1, max2, max3);
 // if(b3 > max ) max = b3;
 // if(c3 > max ) max = c3;
 
-Console.WriteLine(max);
